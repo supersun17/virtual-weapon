4289f4a055e4c670310561fe79bf1f70b23a8b66
Ming Sun <mingsun.nolan@gmail.com>
2021-05-24 11:01:32 +0800

    Add MacOS

---
+++ virtual-weapon-monitor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  virtual-weapon-monitor
+//
+//  Created by Ming Sun on 5/17/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
